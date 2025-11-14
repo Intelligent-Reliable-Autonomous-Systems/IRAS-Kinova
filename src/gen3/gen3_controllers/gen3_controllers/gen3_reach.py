fixed velocity computation bug

src/gen3/gen3_controllers/gen3_controllers/gen3_reach.py
@@ -37,8 +37,8 @@ class Gen3ReachPolicy(PolicyController):
         super().__init__("Gen3ReachPolicy")
 
         self.declare_parameter("model_path", f"{os.getcwd()}/sim2real/policies/reach")
-        self.declare_parameter("target_pos", [0.5, 0.0, 0.2, 0.7071, 0.0, 0.7071, 0.0])
-        # self.declare_parameter("target_pos", [0.6, 0.1, 0.45, 0.7071, 0.0, 0.7071, 0.0])
+        #self.declare_parameter("target_pos", [0.5, 0.0, 0.2, 0.7071, 0.0, 0.7071, 0.0])
+        self.declare_parameter("target_pos", [0.6, 0.1, 0.45, 0.7071, 0.0, 0.7071, 0.0])
         self.model_path = self.get_parameter("model_path").value
         target_pos = self.get_parameter("target_pos").value
 
@@ -71,7 +71,7 @@ class Gen3ReachPolicy(PolicyController):
         obs = np.zeros(2 * self.num_joints + self.targ_cmd_len + self.num_actions)
         obs[: self.num_joints] = self.current_joint_positions - self.default_pos
 
-        obs[self.num_joints : 2 * self.num_joints] = self.current_joint_velocities - self.default_pos
+        obs[self.num_joints : 2 * self.num_joints] = self.current_joint_velocities
 
         obs[2 * self.num_joints : 2 * self.num_joints + self.targ_cmd_len] = command
         obs[2 * self.num_joints + self.targ_cmd_len :] = self._previous_action